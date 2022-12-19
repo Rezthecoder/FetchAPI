e9a199d9f20266244f3b3a99d9124f4ae4f84d1e
Suresh <suresh@gmail.com>
2022-12-19 11:03:59 +0900

    new variable name added

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const data =await fetch('https://api.covid19api.com/summary');
 const res=await data.json();
 console.log(res.Countries);
 datas = document.getElementById('datas');
-datas.innerHTML=`<h1>Country: ${res.Countries[124].Country}</h1>
-<h1>Total Confirmed: ${res.Countries[124].TotalConfirmed}</h1>
-<h1>Total Deaths: ${res.Countries[124].TotalDeaths}</h1>
-<h1>Total Recovered: ${res.Countries[124].TotalRecovered}</h1>
-<h1>New Confirmed: ${res.Countries[124].NewConfirmed}</h1>
-<h1>New Deaths: ${res.Countries[124].NewDeaths}</h1>
-<h1>New Recovered: ${res.Countries[124].NewRecovered}</h1>
-<h1>Date: ${res.Countries[124].Date}</h1>
+const countryName = res.Countries[124];
+datas.innerHTML=`<h1>Country: ${countryName.Country}</h1>
+<h1>Total Confirmed: ${countryName.TotalConfirmed}</h1>
+<h1>Total Deaths: ${countryName.TotalDeaths}</h1>
+<h1>Total Recovered: ${countryName.TotalRecovered}</h1>
+<h1>New Confirmed: ${countryName.NewConfirmed}</h1>
+<h1>New Deaths: ${countryName.NewDeaths}</h1>
+<h1>New Recovered: ${countryName.NewRecovered}</h1>
+<h1>Date: ${countryName.Date}</h1>
 `
 }
 
